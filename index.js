82eefa20c2d3f3b933040e6d224a247a233988f3
Emily DuBois <emilymdubois@gmail.com>
2017-08-10 16:40:35 -0400

    Make headers fancy again

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ const WebhookPassthroughMethod = (lambda) => ({
       ],
       Uri: cf.sub(`arn:aws:apigateway:\${AWS::Region}:lambda:path/2015-03-31/functions/\${${lambda}.Arn}/invocations`),
       RequestTemplates: {
-        'application/json': "{\"headers\":\"$input.params()\",\"body\":$input.json('$'),\"method\":\"$context.httpMethod\"}"
+        'application/json': "\"headers\":{\n #foreach($header in $input.params().header.keySet())\n \"$header\": \"$util.escapeJavaScript($input.params().header.get($header))\" #if($foreach.hasNext),#end\n \n #end\n },\"body\":$input.json('$'),\"method\":\"$context.httpMethod\"}"
       }
     },
     MethodResponses: [
